d01fc16b9058f21500569e45487656171fc0d62c
V.A.K.M.Nethmina <cst21022@std.uwu.ac.lk>
2026-04-24 13:01:24 +0530

    Updated

---
+++ src/main/java/com/example/Budget/Tracking/Application/Backend/DTO/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.example.Budget.Tracking.Application.Backend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
