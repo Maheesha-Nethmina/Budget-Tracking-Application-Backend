7ddf8f4e78d540f504a8653b4b7e5baad63a77e4
V.A.K.M.Nethmina <cst21022@std.uwu.ac.lk>
2026-04-24 11:28:03 +0530

    first commit

---
+++ src/main/java/com/example/Budget/Tracking/Application/Backend/BudgetTrackingApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Budget.Tracking.Application.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackingApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetTrackingApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Budget/Tracking/Application/Backend/BudgetTrackingApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Budget.Tracking.Application.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackingApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
