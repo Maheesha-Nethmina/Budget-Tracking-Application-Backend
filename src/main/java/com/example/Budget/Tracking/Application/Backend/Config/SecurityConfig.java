d17dd84a2a85f498f5ac5e070d82bc53799551f1
V.A.K.M.Nethmina <cst21022@std.uwu.ac.lk>
2026-04-25 19:56:33 +0530

    updated

--- src/main/java/com/example/Budget/Tracking/Application/Backend/Config/SecurityConfig.java
+++ src/main/java/com/example/Budget/Tracking/Application/Backend/Config/SecurityConfig.java
@@ -2,7 +2,7 @@ package com.example.Budget.Tracking.Application.Backend.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod; // IMPORTANT NEW IMPORT
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
